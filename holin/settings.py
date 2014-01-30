26905b0fe901eb80d78bc475d3fce67ed70c8ba1
Vasiliy Stavenko <stavenko@gmail.com>
2014-01-30 13:37:19 +1200

    1

--- holin/settings.py
+++ holin/settings.py
@@ -175,3 +175,12 @@ LOGGING = {
         },
     }
 }
+
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
